09b04f82ff1fce8aff3e8e545037f57fd1d29a2e
Josh Kneeland <josh@joshkneeland.com>
2021-06-05 14:04:15 -0600

    Added components directory and TabsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoComponent } from './components/demo/demo.component';
+import { ApiComponent } from './components/api/api.component';
+import { SpecComponent } from './components/spec/spec.component';
+import { AccessibilityComponent } from './components/accessibility/accessibility.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    ApiComponent,
+    SpecComponent,
+    AccessibilityComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
